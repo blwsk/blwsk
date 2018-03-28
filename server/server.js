4c73c3a4f7a8deb3cae31d5e0198433fb2c2c3d9
Kevin Bielawski <kbielawski@hubspot.com>
2018-03-27 21:26:20 -0400

    it runs

--- server/server.js
+++ server/server.js
@@ -1,32 +1,30 @@
-'use strict';
-require('babel-core/register');
+"use strict";
 
-var express = require('express');
-var compression = require('compression');
-var serveStatic = require('serve-static');
-var favicon = require('serve-favicon');
-var bodyParser = require('body-parser');
+const express = require("express");
+const compression = require("compression");
+const serveStatic = require("serve-static");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 
-app.set('views', './static')
-app.set('view engine', 'html');
-app.engine('html', require('hbs').__express);
+app.set("views", "./static");
+app.set("view engine", "html");
+app.engine("html", require("hbs").__express);
 
-app.use(favicon('static/favicon.ico'));
+app.use(favicon("static/favicon.ico"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
-app.use(serveStatic('build'));
+app.use(serveStatic("build"));
 
 // sessions
-app.use(require('./db').sessionFunc);
+app.use(require("./db").sessionFunc);
 
 // routes
-app.use(require('./router'));
+app.use(require("./router"));
 
-
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log('Listening on ' + port);
+  console.log("Listening on " + port);
 });
